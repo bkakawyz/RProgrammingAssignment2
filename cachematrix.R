d76be0cfbc0edb062071067ee04ee806e0cb64ee
bkakawyz <bskumar80@gmail.com>
2016-08-31 23:59:00 -0400

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,38 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+## Write a short comment describing this function
+## The function prints the inverse of a matrix. If an inverse has
+## already been calculated, then the function gets the inverted result
+## and skips the process. If else, the function computes the inverse
+## and stores it in the cache
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
+git remote add origin https://github.com/bkakawyz/RProgrammingAssignment2.git
+git push -u origin master
